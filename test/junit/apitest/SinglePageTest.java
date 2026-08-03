b16076a8648803008201bfad6e4999f5bec76366
Hannes Wallnoefer <hannes.wallnoefer@oracle.com>
2026-08-03 17:25:25 +0200

    Improve single page report test

--- test/junit/apitest/SinglePageTest.java
+++ test/junit/apitest/SinglePageTest.java
@@ -87,16 +87,73 @@ public class SinglePageTest extends APITester {
 
         run(options);
 
-        Path allChanges = outDir.resolve("m").resolve("p").resolve("all-changes.html");
-        Assertions.assertTrue(Files.exists(allChanges), "all-changes.html not found");
-        checkOutput(allChanges, "Changed", "Added", "Removed");
-        checkOutput(allChanges, "package-summary.html", "View changes in multiple pages");
-
-        Path packageSummary = outDir.resolve("m").resolve("p").resolve("package-summary.html");
-        checkOutput(packageSummary, "all-changes.html", "View changes in one page");
+        checkFile(outDir.resolve("index.html"), false);
+        checkFile(outDir.resolve("all-changes.html"), true);
+        checkFile(outDir.resolve("m").resolve("module-summary.html"), false);
+        checkFile(outDir.resolve("m").resolve("all-changes.html"), true);
+        checkFile(outDir.resolve("m").resolve("p").resolve("package-summary.html"), false);
+        checkFile(outDir.resolve("m").resolve("p").resolve("all-changes.html"), true);
 
         Path allChangesUnchanged = outDir.resolve("m").resolve("q").resolve("all-changes.html");
         Assertions.assertTrue(Files.exists(allChangesUnchanged),
                 "all-changes.html not found in unchanged package");
+    }
+
+    private void checkFile(Path file, boolean isSinglePage) throws IOException {
+        Assertions.assertTrue(Files.exists(file), "File " + file + " not found");
+
+        if (isSinglePage) {
+            checkOutput(file, "Overview", "View changes in multiple pages");
+            checkOutput(file, """
+                    <section class="changed-element" id="id-m/p"><h2><a href="#id-m/p">Package p</a></h2>
+                    <div class="changed-type-content"><div class="element"><span class="diff">&ne;</span><div class="signature"><span class="keyword">package</span> p</div>
+                    </div>
+                    <section class="enclosed"><h3>Types</h3>
+                    <ul><li><span><span class="add">&plus;</span> <a href="#id-m/p.Added">Added</a></span></li><li><span><span class="diff">&ne;</span> <a href="#id-m/p.Changed">Changed</a></span></li><li><span><span class="remove">&minus;</span> <a href="#id-m/p.Removed">Removed</a></span></li><li class="unchanged"><span><span class="same">&equals;</span> <a href="#id-m/p.Same">Same</a></span></li></ul>
+                    </section>
+                    </div>
+                    </section>
+                    <section class="changed-element" id="id-m/p.Added"><h2><a href="#id-m/p.Added">Class Added</a></h2>
+                    <div class="changed-type-content"><div class="element"><span class="add">&plus;</span><span class="missing missing-caption-add">Only in: api1; not in: api0.</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> <span class="keyword">class</span> Added <div class="superclass"><span class="keyword">extends</span> java.lang.Object</div>
+                    </div>
+                    </div>
+                    <section class="enclosed"><h3>Constructors</h3>
+                    <ul><li><div class="element" id="&lt;init&gt;()"><span class="add">&plus;</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> Added()</div>
+                    </div>
+                    </li></ul>
+                    </section>
+                    </div>
+                    </section>
+                    <section class="changed-element" id="id-m/p.Changed"><h2><a href="#id-m/p.Changed">Class Changed</a></h2>
+                    <div class="changed-type-content"><div class="element"><span class="diff">&ne;</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> <span class="keyword">class</span> Changed <div class="superclass"><span class="keyword">extends</span> java.lang.Object</div>
+                    </div>
+                    </div>
+                    <section class="enclosed"><h3>Fields</h3>
+                    <ul><li><div class="element" id="n"><span class="diff">&ne;</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> <span class="diffs"><span class="api" title="api0"><span class="keyword">int</span></span><span class="api" title="api1"><span class="keyword">long</span></span></span> n</div>
+                    </div>
+                    </li></ul>
+                    </section>
+                    <div class="unchanged"><section class="enclosed"><h3>Constructors</h3>
+                    <ul><li><div class="element" id="&lt;init&gt;()"><span class="same">&equals;</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> Changed()</div>
+                    </div>
+                    </li></ul>
+                    </section>
+                    </div>
+                    </div>
+                    </section>
+                    <section class="changed-element" id="id-m/p.Removed"><h2><a href="#id-m/p.Removed">Class Removed</a></h2>
+                    <div class="changed-type-content"><div class="element"><span class="remove">&minus;</span><span class="missing missing-caption-remove">Only in: api0; not in: api1.</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> <span class="keyword">class</span> Removed <div class="superclass"><span class="keyword">extends</span> java.lang.Object</div>
+                    </div>
+                    </div>
+                    <section class="enclosed"><h3>Constructors</h3>
+                    <ul><li><div class="element" id="&lt;init&gt;()"><span class="remove">&minus;</span><div class="signature"><span class="modifiers"><span class="keyword">public</span></span> Removed()</div>
+                    </div>
+                    </li></ul>
+                    </section>
+                    </div>
+                    </section>""");
+        } else {
+            checkOutput(file, "Overview", "all-changes.html", "View changes in one page");
+        }
     }
 }
